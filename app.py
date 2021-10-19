bf8ec479de32f3442f2580e90028fea379c9c5b7
daniellima338 <daniellima338@gmail.com>
2021-10-19 13:27:28 +0000

    added route to registration page

--- app.py
+++ app.py
@@ -23,6 +23,28 @@ def get_descriptions():
     descriptions = list(mongo.db.country_description.find())
     return render_template("descriptions.html", descriptions=descriptions)
 
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        #check if username already exists in db
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+        if existing_user:
+            flash("Username already exists")
+            return redirect(url_for("register"))
+
+        register = {
+            "username": request.form.get("username").lower(),
+            "password": generate_password_hash(request.form.get("password"))
+        }
+        mongo.db.users.insert_one(register)
+
+        #put the new user into "session" cookie
+        session["user"] = request.form.get("username").lower()
+        flash("Registration Successful!")
+        return redirect(url_for("profile", username=session["user"]))
+    return render_template("register.html")
+
 
 if __name__ == "__main__":
     app.run(
