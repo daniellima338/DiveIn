c3d81dc6a216beec7c2d8b6b790a85643faab5ca
daniellima338 <daniellima338@gmail.com>
2021-11-01 12:35:00 +0000

    Home page visuals modified

--- static/js/index.js
+++ static/js/index.js
@@ -3,13 +3,4 @@ let map;
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
     center: { lat: -34.397, lng: 150.644 },
-    zoom: 8,
-  });
-}
-
-function initMap() {
-    map = new google.maps.Map(document.getElementById("map1"), {
-      center: { lat: -34.397, lng: 150.644 },
-      zoom: 8,
-    });
-  }
+    zoom: 8})};
