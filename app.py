86d3f61a8f033c54bf04eba066a5107e9d2fff3c
daniellima338 <daniellima338@gmail.com>
2021-11-03 21:50:26 +0000

    added logout functionality. Added seperation in navbar between being logged in and logged out

--- app.py
+++ app.py
@@ -87,7 +87,19 @@ def profile(username):
     # grab the session user's username from db
     username = mongo.db.users.find_one(
         {"username": session["user"]})["username"]
-    return render_template("pages/profile.html", username=username)
+
+    if session["user"]:
+        return render_template("pages/profile.html", username=username)
+
+    return redirect(url_for("home"))
+
+
+@app.route("/logout")
+def logout():
+    #remove user from session cookies
+    flash("You have been logged out")
+    session.pop("user")
+    return redirect(url_for("home"))
 
 if __name__ == "__main__":
     app.run(
